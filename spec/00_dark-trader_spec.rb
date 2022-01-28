7d07e078982bce825e02de892a3c640c5e944a38
Teacher A <teaching.know@gmail.com>
2022-01-28 08:01:42 +0100

    Update 00_dark-trader_spec.rb

--- spec/00_dark-trader_spec.rb
+++ spec/00_dark-trader_spec.rb
@@ -1,2 +1,22 @@
+require_relative "../lib/crypto_scrapping"
+
 require 'nokogiri'
 require 'open-uri'
+
+
+
+describe "make_hash method, it should return a hash with both arrays" do
+  it "Wrong type" do
+    expect(make_hash("","")).to eq(nil)
+  end
+end
+
+
+describe "open_url method, takes a url, return the html" do
+  it "TEST empty url" do
+    expect(open_url("")).to eq(nil)
+  end
+  it "TEST good url" do
+    expect(open_url("https://www.google.com/")).to be_kind_of(Nokogiri::HTML::Document)
+  end
+end
