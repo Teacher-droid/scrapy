05635a800875f2dbc0d938f77e12d5c02a95c0b2
Teacher A <teaching.know@gmail.com>
2022-01-28 08:38:46 +0100

    Update 00_dark-trader_spec.rb

--- spec/00_dark-trader_spec.rb
+++ spec/00_dark-trader_spec.rb
@@ -1,17 +1,18 @@
+# je sollicite le fichier dark-trader
 require_relative "../lib/dark-trader"
 
 require 'nokogiri'
 require 'open-uri'
 
 
-
+# je teste la génération d'un hash contenant les deux tableaux de nom et de valeur
 describe "make_hash method, it should return a hash with both arrays" do
   it "Wrong type" do
     expect(make_hash("","")).to eq(nil)
   end
 end
 
-
+# je teste la récupération d'un url pour renvoyer un html
 describe "open_url method, takes a url, return the html" do
   it "TEST empty url" do
     expect(open_url("")).to eq(nil)
